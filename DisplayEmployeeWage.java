c348e0773375eaef84816baa72f4b5385adf5986
unknown <shravyak094@gmail.com>
2020-09-11 16:41:04 +0530

    Added: DisplayEmployeeWage.java file

---
+++ DisplayEmployeeWage.java
@@ -0,0 +1,10 @@
+public class DisplayEmployeeWage {
+
+	public static void main(String[] args) {
+
+		ComputeWage computeWage = new ComputeWage();
+		System.out.println(computeWage.computingWage());
+
+	}
+
+}
