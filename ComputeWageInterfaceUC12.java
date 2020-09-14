963f33071a34ce6bee7538a4037c675bb950a31f
Shravya <70620812+ShravyaK-Hub@users.noreply.github.com>
2020-09-14 07:24:02 -0700

    Delete ComputeWageInterfaceUC12.java

--- ComputeWageInterfaceUC12.java
+++
@@ -1,7 +0,0 @@
-
-public interface ComputeWageInterfaceUC12 {
-
-	void addCompany(String companyName, int wagePerHour, int totalDays, int totalHours);
-	void setWage();
-	void computeWage(CompanyInfoUC12 computeWage);
-}
